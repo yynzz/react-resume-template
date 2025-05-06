5b1468328319b5e2e1e0d9f071928cb767e4abda
yynzz <86478022+yynzz@users.noreply.github.com>
2025-05-06 21:14:49 +0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,23 +3,35 @@ import '../globalStyles.scss';
 
 import type {AppProps} from 'next/app';
 import {memo, useEffect} from 'react';
-
-
+import {useRouter} from 'next/router';
 
 
 const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
   
+  const router = useRouter();
+  
   useEffect(() => {
-    // 檢查 URL 是否包含錨點
-    if (window.location.hash) {
-      // 先滾動到頂部
+    const handleRouteChange = () => {
+      // 立即執行滾動重置，不使用延時
       window.scrollTo(0, 0);
       
-      // 使用 history API 清除錨點但不重新加載頁面
-      const cleanUrl = window.location.pathname + window.location.search;
-      window.history.replaceState({}, document.title, cleanUrl);
-    }
-  }, []);
+      // 清除錨點
+      if (window.location.hash) {
+        const cleanUrl = window.location.pathname + window.location.search;
+        window.history.replaceState({}, document.title, cleanUrl);
+      }
+    };
+    
+    // 頁面初始加載時重置滾動
+    handleRouteChange();
+    
+    // 添加路由事件監聽器
+    router.events.on('routeChangeComplete', handleRouteChange);
+    
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router]);
 
   return (
     <>
