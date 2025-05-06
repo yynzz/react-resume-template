ec14bd7a6fc2d597d036799a7a93cbb2d0e7458f
yynzz <86478022+yynzz@users.noreply.github.com>
2025-05-06 21:02:44 +0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,25 @@ import 'tailwindcss/tailwind.css';
 import '../globalStyles.scss';
 
 import type {AppProps} from 'next/app';
-import {memo} from 'react';
+import {memo, useEffect} from 'react';
+
+
+
 
 const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
+  
+  useEffect(() => {
+    // 檢查 URL 是否包含錨點
+    if (window.location.hash) {
+      // 先滾動到頂部
+      window.scrollTo(0, 0);
+      
+      // 使用 history API 清除錨點但不重新加載頁面
+      const cleanUrl = window.location.pathname + window.location.search;
+      window.history.replaceState({}, document.title, cleanUrl);
+    }
+  }, []);
+
   return (
     <>
       <Component {...pageProps} />
